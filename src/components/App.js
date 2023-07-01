1c121d1232ee3e6637fa170bd7120847585b09f6
CrisMuniz <cristinamuniz85@gmail.com>
2023-07-01 13:01:05 +0200

    add starter kit

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div>Hola mundo</div>
+  );
+}
+
+export default App;
+
+// Fichero src/components/App.js
+// Fichero src/components/App.js
+
+// import {Link, Route, Routes} from 'react-router-dom';
+
+// const App = () => {
+//   return(
+//     <div>
+//       <h2>Este titulo aparece siempre</h2>
+//       <Routes>
+//         <Route
+//         path='/contacto'
+//         element={
+//           <h2>
+//             Este titulo solo aparece cuando la usuaria entra en la pagina de contacto
+//           </h2>
+//         }/>
+//       </Routes>
+//       <nav>
+//         <ul>
+//           <li>
+//           <Link to='/'>Ir al inicio</Link>
+//             {/* <a href="/#/">Ir al inicio</a> */}
+//           </li>
+//           <li>
+//           <Link to='/contacto'>Ir a contacto</Link>
+//             {/* <a href="/#/contacto">Ir a contacto</a> */}
+//           </li>
+//         </ul>
+//       </nav>
+//     </div>
+//   )
+// }
+
+// export default App;
